a20527cd0cafe5c82d94fc0f137316b5e20cb882
gan11a <ivan.tulskuy@yahoo.com>
2021-09-02 00:21:31 +0300

    created TextBoxTests

---
+++ src/test/java/studentguru/qa/tests/TextBoxTests.java
@@ -0,0 +1,35 @@
+package studentguru.qa.tests;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TextBoxTests {
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.startMaximized=true;
+    }
+
+    @Test
+    void fillFormTest(){
+        open("https://demoqa.com/text-box");
+        $("#userName").setValue("gan1a");
+        $("#userEmail").setValue("gan1a@test.com");
+        $("#currentAddress").setValue("Pushkina/Kolotushkina");
+        $("#permanentAddress").setValue("Kolotushkina/Pushkina");
+        $("#submit").click();
+
+
+        $("#output #name").shouldHave(text("gan1a"));
+        $("#output #email").shouldHave(text("gan1a@test.com"));
+        $("#output #currentAddress").shouldHave(text("Pushkina/Kolotushkina"));
+        $("#output #permanentAddress").shouldHave(text("Kolotushkina/Pushkina"));
+
+
+    }
+}
