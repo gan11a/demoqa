e7565fa78d252466addaa713c90b85402dad67ad
gan11a <ivan.tulskuy@yahoo.com>
2021-09-01 23:22:37 +0300

    added classes & bigger junit test

---
+++ src/test/java/studentguru/qa/DemoQAStudentTest.java
@@ -0,0 +1,30 @@
+package studentguru.qa;
+
+import org.junit.jupiter.api.*;
+
+public class DemoQAStudentTest {
+    @BeforeAll
+    static void beforeAll(){
+        System.out.println("Before all worlds!");
+    }
+    @AfterAll
+    static void afterAll(){
+        System.out.println("After All Worlds!");
+    }
+    @BeforeEach
+    void beforeEach(){
+        System.out.println("Before World!");
+    }
+    @AfterEach
+    void afterEach(){
+        System.out.println("After World!");
+    }
+    @Test
+    void firstTest() {
+        System.out.println("Hello World");
+    }
+    @Test
+    void secondTest() {
+        System.out.println("Hello World (second)");
+    }
+}
